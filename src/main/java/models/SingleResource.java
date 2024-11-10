4bb078fea6a5c5e00b3fa213aa1e0a9705428ba9
Sergei Begletsov <snower87@yandex.ru>
2024-11-10 07:20:08 +0500

    add data for solutions to 2 task

---
+++ src/main/java/models/SingleResource.java
@@ -0,0 +1,22 @@
+package models;
+
+public class SingleResource {
+    ColorData colorData;
+    Support support;
+
+    public ColorData getData() {
+        return colorData;
+    }
+
+    public void setData(ColorData colorData) {
+        this.colorData = colorData;
+    }
+
+    public Support getSupport() {
+        return support;
+    }
+
+    public void setSupport(Support support) {
+        this.support = support;
+    }
+}
